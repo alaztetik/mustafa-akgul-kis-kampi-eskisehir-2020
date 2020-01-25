96d23b32af0bd84dfd7c93fb4cd178a1ade73ccc
Alaz Tetik <alaztetik@gmail.com>
2020-01-25 09:33:15 +0300

    Add code snippets

--- before/ruby/ruby-learning.com/p002rubynumbers.rb
+++ before/ruby/ruby-learning.com/p002rubynumbers.rb
@@ -6,8 +6,48 @@ Usual operators:
 + - * / 
 =end
 
+=begin
 puts 1+2
 puts 2*3
 puts 3/2
 puts 10-20
 puts 1.5 / 2.7
+=end
+
+=begin
+puts (5%3)
+puts (-5%3)
+puts (5%-3)
+puts (-5%-3)
+=end
+
+=begin
+puts nil || 2020
+puts false || 2020
+puts "ruby" || 2020
+=end
+
+=begin
+@variable = @variable || "default value"
+puts @variable
+=end
+
+=begin 
+@variable ||= "default value"
+puts @variable
+=end
+
+=begin 
+a=1
+b=2
+c=1
+
+if a == 1 and b == 2 and c == 3 
+  then puts "True"
+else 
+  puts "False"
+end
+=end
+
+puts `ls`
+puts `pwd`
